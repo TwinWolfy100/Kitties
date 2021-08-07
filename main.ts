a0de526f24e451bf4f665dc9b5be896fdbbbea37
TwinWolfy100 <88583397+TwinWolfy100@users.noreply.github.com>
2021-08-07 15:10:10 +0100

    I added an enemy and removed the follows as it was irritating.

--- main.ts
+++ main.ts
@@ -1,6 +1,69 @@
 namespace SpriteKind {
     export const Follower = SpriteKind.create()
 }
+function setUpBee () {
+    for (let index = 0; index < 3; index++) {
+        myEnemy = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . c c c c . . . . . . . . 
+            . . c c 5 5 5 5 c c . . . . . . 
+            . c 5 5 5 5 5 5 5 5 c . . . . . 
+            c 5 5 5 5 5 1 f 5 5 5 c . . . . 
+            c 5 5 5 5 5 f f 5 5 5 5 c . . . 
+            c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
+            c c b b 1 b 5 5 5 5 5 5 d c . . 
+            c 5 3 3 3 5 5 5 5 5 d d d c . . 
+            . b 5 5 5 5 5 5 5 5 d d d c . . 
+            . . c b b c 5 5 b d d d d c c . 
+            . c b b c 5 5 b b d d d d c d c 
+            . c c c c c c d d d d d d d d c 
+            . . . c c c c d 5 5 b d d d c . 
+            . . c c c c c b 5 5 b c c c . . 
+            . . c b b b c d 5 5 b c . . . . 
+            `, SpriteKind.Enemy)
+        tiles.placeOnRandomTile(myEnemy, sprites.castle.tilePath5)
+        animation.runImageAnimation(
+        myEnemy,
+        [img`
+            . . . . c c c c c . . . . . . . 
+            . . c c 5 5 5 5 5 c . . . . . . 
+            . c 5 5 5 5 1 f 5 5 c . . . . . 
+            c 5 5 5 5 5 f f 5 5 5 c . . . . 
+            c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
+            c c b b 1 b 5 5 5 5 5 5 c . . . 
+            c 5 3 3 3 5 5 5 5 5 5 5 d c . . 
+            c 5 3 3 3 5 5 5 5 5 d d d c . . 
+            . c 5 5 5 5 b 5 5 5 d d d c . . 
+            . . c b b c 5 5 b d d d d c . . 
+            . c b b c 5 5 b b d d d d c c c 
+            . c c c c c c d d d d d d d d c 
+            . . . . c c c b 5 5 b d d d c . 
+            . . . . . c d 5 5 b b c c c . . 
+            . . . . c c c c c c c . . . . . 
+            . . . . c b b b c . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . c c c c . . . . . . . . 
+            . . c c 5 5 5 5 c c . . . . . . 
+            . c 5 5 5 5 5 5 5 5 c . . . . . 
+            c 5 5 5 5 5 1 f 5 5 5 c . . . . 
+            c 5 5 5 5 5 f f 5 5 5 5 c . . . 
+            c c b b 1 b 5 5 5 5 5 5 c . . . 
+            c c 3 3 b b 5 5 5 5 5 5 d c . . 
+            c 5 3 3 3 5 5 5 5 5 d d d c . . 
+            . b 5 5 5 5 5 5 5 5 d d d c . . 
+            . . c b b c 5 5 b d d d d c . . 
+            . c b b c 5 5 b b d d d d c c c 
+            . c c c c c c d d d d d d d d c 
+            . . . c c c c d 5 5 b d d d c c 
+            . . . c b c c b 5 5 b c c c . . 
+            . . . c c c d 5 5 b c . . . . . 
+            `],
+        100,
+        true
+        )
+    }
+}
 function setUpStars () {
     for (let index = 0; index < 10; index++) {
         star = sprites.create(img`
@@ -125,6 +188,7 @@ sprites.onOverlap(SpriteKind.Food, SpriteKind.Player, function (sprite, otherSpr
     }
 })
 function setUpFollower (sprite: Sprite, speed: number) {
+    let Follower: Sprite = null
     sprite.follow(Leader, speed)
     Follower.say(":)", 3000)
 }
@@ -206,174 +270,17 @@ function setUpPlayers () {
     )
     Leader.setPosition(21, 97)
     controller.moveSprite(Leader)
-    Duck = sprites.create(img`
-        . . . . . . . . . . b 5 b . . . 
-        . . . . . . . . . b 5 b . . . . 
-        . . . . . . b b b b b b . . . . 
-        . . . . . b b 5 5 5 5 5 b . . . 
-        . . . . b b 5 d 1 f 5 d 4 c . . 
-        . . . . b 5 5 1 f f d d 4 4 4 b 
-        . . . . b 5 5 d f b 4 4 4 4 b . 
-        . . . b d 5 5 5 5 4 4 4 4 b . . 
-        . . b d d 5 5 5 5 5 5 5 5 b . . 
-        . b d d d d 5 5 5 5 5 5 5 5 b . 
-        b d d d b b b 5 5 5 5 5 5 5 b . 
-        c d d b 5 5 d c 5 5 5 5 5 5 b . 
-        c b b d 5 d c d 5 5 5 5 5 5 b . 
-        . b 5 5 b c d d 5 5 5 5 5 d b . 
-        b b c c c d d d d 5 5 5 b b . . 
-        . . . c c c c c c c c b b . . . 
-        `, SpriteKind.Follower)
-    Duck.follow(Leader, 35)
-    Other_Car = sprites.create(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . . f e e d d d d d d e e f . . 
-        . . . f e e 4 4 4 4 e e f . . . 
-        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
-        `, SpriteKind.Follower)
     scene.cameraFollowSprite(Leader)
-    tiles.placeOnTile(Other_Car, tiles.getTileLocation(3, 5))
-    Other_Car.follow(Leader, 30)
-    Follower = sprites.create(img`
-        . . . . . . 5 . 5 . . . . . . . 
-        . . . . . f 5 5 5 f f . . . . . 
-        . . . . f 1 5 2 5 1 6 f . . . . 
-        . . . f 1 6 6 6 6 6 1 6 f . . . 
-        . . . f 6 6 f f f f 6 1 f . . . 
-        . . . f 6 f f d d f f 6 f . . . 
-        . . f 6 f d f d d f d f 6 f . . 
-        . . f 6 f d 3 d d 3 d f 6 f . . 
-        . . f 6 6 f d d d d f 6 6 f . . 
-        . f 6 6 f 3 f f f f 3 f 6 6 f . 
-        . . f f d 3 5 3 3 5 3 d f f . . 
-        . . f d d f 3 5 5 3 f d d f . . 
-        . . . f f 3 3 3 3 3 3 f f . . . 
-        . . . f 3 3 5 3 3 5 3 3 f . . . 
-        . . . f f f f f f f f f f . . . 
-        . . . . . f f . . f f . . . . . 
-        `, SpriteKind.Follower)
-    setUpFollower(Follower, 40)
-    Follower = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . 4 4 4 . . . . 4 4 4 . . . . 
-        . 4 5 5 5 e . . e 5 5 5 4 . . . 
-        4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
-        4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
-        e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
-        . e e 5 5 5 5 5 5 5 5 e e . . . 
-        . . e 5 f 5 5 5 5 f 5 e . . . . 
-        . . f 5 5 5 4 4 5 5 5 f . f f . 
-        . . . 4 5 5 f f 5 5 6 f f 5 f . 
-        . . . f 6 6 6 6 6 6 4 4 4 5 f . 
-        . . . f 5 5 5 5 5 5 5 f f f . . 
-        . . . f 5 4 5 f f f 5 f . . . . 
-        . . . f f f f f . . f f . . . . 
-        `, SpriteKind.Follower)
-    setUpFollower(Follower, 50)
-    Follower = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . c c c c . . . . 
-        . . . . . . c c d d d d c . . . 
-        . . . . . c c c c c c d c . . . 
-        . . . . c c 4 4 4 4 d c c . . . 
-        . . . c 4 d 4 4 4 4 4 1 c . c c 
-        . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
-        . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
-        f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
-        f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
-        f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
-        . f 4 4 4 4 1 c 4 f 4 d f f f f 
-        . . f f 4 d 4 4 f f 4 c f c . . 
-        . . . . f f 4 4 4 4 c d b c . . 
-        . . . . . . f f f f d d d c . . 
-        . . . . . . . . . . c c c . . . 
-        `, SpriteKind.Follower)
-    setUpFollower(Follower, 70)
-    Follower = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . c c c c c . . . 
-        . . . . . . c c 5 5 5 5 5 c . . 
-        . . . . . c 5 5 5 5 5 5 5 5 c . 
-        . . . . c b b b b b b 5 5 5 c . 
-        . . . . c 1 1 b b 1 b b c c . . 
-        . . . c 1 1 1 b b 1 1 1 c . . . 
-        . . . c 1 1 1 1 b 1 1 1 c . c c 
-        . . . c d 1 1 1 b 1 1 1 b b 5 c 
-        . . c c d 1 c 1 b 1 b 1 5 5 5 c 
-        . c c d d 1 1 1 1 1 b 1 b b 5 c 
-        f d d d 1 1 1 1 1 b b 1 f . c c 
-        f f f 1 1 1 1 1 1 b b b f . . . 
-        . . . f f 1 1 1 b b b 5 5 f . . 
-        . . . . . f f f 5 5 5 5 5 f . . 
-        . . . . . . . . f f f f f f . . 
-        `, SpriteKind.Follower)
-    setUpFollower(Follower, 80)
-    Follower = sprites.create(img`
-        e e e . . . . e e e . . . . 
-        c d d c . . c d d c . . . . 
-        c b d d f f d d b c . . . . 
-        c 3 b d d b d b 3 c . . . . 
-        f b 3 d d d d 3 b f . . . . 
-        e d d d d d d d d e . . . . 
-        e d f d d d d f d e . b f b 
-        f d d f d d f d d f . f d f 
-        f b d d b b d d 2 f . f d f 
-        . f 2 2 2 2 2 2 b b f f d f 
-        . f b d d d d d d b b d b f 
-        . f d d d d d b d d f f f . 
-        . f d f f f d f f d f . . . 
-        . f f . . f f . . f f . . . 
-        `, SpriteKind.Follower)
-    setUpFollower(Follower, 90)
-}
-function setUpShark () {
-    Shark = sprites.create(img`
-        .............ccfff..............
-        ...........ccddbcf..............
-        ..........ccddbbf...............
-        ..........fccbbcf...............
-        .....fffffccccccff.........ccc..
-        ...ffbbbbbbbcbbbbcfff....ccbbc..
-        ..fbbbbbbbbcbcbbbbcccff.cdbbc...
-        ffbbbbbbffbbcbcbbbcccccfcdbbf...
-        fbcbbb11ff1bcbbbbbcccccffbbf....
-        fbbb11111111bbbbbcccccccbbcf....
-        .fb11133cc11bbbbcccccccccccf....
-        ..fccc31c111bbbcccccbdbffbbcf...
-        ...fc13c111cbbbfcddddcc..fbbf...
-        ....fccc111fbdbbccdcc.....fbbf..
-        ........ccccfcdbbcc........fff..
-        .............fffff..............
-        `, SpriteKind.Enemy)
-    random = randint(10, 150)
-    Shark.setPosition(159, random)
-    Shark.setVelocity(-100, 0)
-    Shark.setFlag(SpriteFlag.DestroyOnWall, true)
+    setUpBee()
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     game.over(false, effects.dissolve)
 })
-let random = 0
-let Shark: Sprite = null
-let Other_Car: Sprite = null
-let Duck: Sprite = null
-let Follower: Sprite = null
 let Leader: Sprite = null
 let levelNumber = 0
 let scoreRemainderAfterDivisionBy10 = 0
 let star: Sprite = null
+let myEnemy: Sprite = null
 info.setScore(0)
 tiles.setTilemap(tilemap`level2`)
 setUpPlayers()
